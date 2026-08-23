fb6ae41eddd417d7530fe92a440d0ab808d9f3b8
AmmonNucor <88739319+AmmonNucor@users.noreply.github.com>
2026-08-23 17:38:40 +0000

    Move Ko-fi widget to client component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+import KofiWidget from './components/KofiWidget'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
